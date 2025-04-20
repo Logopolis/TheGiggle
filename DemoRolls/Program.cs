5108b5e8bc6ba470ed3eca79c4d9b74f7c50e835
Chris Segrove <myantipodeanpenpal@gmail.com>
2025-04-20 12:01:58 +0100

    Add DiceRolls and Demo project, with first pass implementations

---
+++ DemoRolls/Program.cs
@@ -0,0 +1,20 @@
+﻿using TheGiggle;
+
+foreach (var d in new List<DiceRoll>([
+    new DiceRoll(2, 6, 0),
+    new DiceRoll(2, 12, 0),
+    new DiceRoll(2, 12, 1),
+    new DiceRoll(2, 12, 2),
+    new DiceRoll(2, 12, 3),
+    new DiceRoll(2, 12, 4),
+    new DiceRoll(2, 12, 5),
+    new DiceRoll(2, 12, 6),
+]))
+{ RollToConsole(d); }
+
+Console.WriteLine($"{DiceRoll.Roll(20, 1, 10)} should be 30");
+
+void RollToConsole(DiceRoll diceRoll)
+{
+    Console.WriteLine($"{diceRoll.NumberOfDice}d{diceRoll.DiceType}+{diceRoll.Bonus} [{diceRoll.DiceCheat}] = {diceRoll.GetResult()}");
+}
---
+++ TheGiggle/DiceRoll.cs
@@ -0,0 +1,59 @@
+﻿namespace TheGiggle;
+
+public class DiceRoll
+{
+    public int NumberOfDice { get; }
+    public int DiceType { get; }
+    public int Bonus { get; }
+    public DiceCheat DiceCheat { get; }
+
+    protected static Random Random = new Random();
+
+    public DiceRoll(
+        int numberOfDice,
+        int diceType,
+        int bonus,
+        DiceCheat diceCheat = DiceCheat.None)
+    {
+        NumberOfDice = numberOfDice;
+        DiceType = diceType;
+        Bonus = bonus;
+        DiceCheat = diceCheat;
+    }
+
+    public static int Roll(
+        int NumberOfDice,
+        int DiceType,
+        int Bonus,
+        DiceCheat DiceCheat = DiceCheat.None)
+    {
+        return new DiceRoll(DiceType, NumberOfDice, Bonus, DiceCheat)
+            .GetResult();
+    }
+
+    public virtual int GetResult()
+    {
+        var result = 0;
+        for (int i = 0; i < NumberOfDice; i++)
+        {
+            result += RollOneDie();
+        }
+
+        return result + Bonus;
+    }
+
+    protected virtual int RollOneDie() => DiceCheat switch
+    { 
+        DiceCheat.None => Random.Next(DiceType) + 1,
+        DiceCheat.MaxValue => DiceType,
+        DiceCheat.MinValue => 1,
+        _ => throw new NotImplementedException(DiceCheat.ToString())
+    };
+}
+
+public enum DiceCheat
+{
+    None,
+    MaxValue,
+    MinValue
+}
